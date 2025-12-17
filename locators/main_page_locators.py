ee03e1709e29e12c8dc6dcdf719995482060b845
emilkabals <esamigyllin@gmail.com>
2025-12-18 01:23:37 +0300

    локаторы исправлены, добавлен поиск по тексту

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -1,36 +1,36 @@
 from selenium.webdriver.common.by import By
 
-
 class MainPageLocators:
+    """Устойчивые локаторы для главной страницы Яндекс.Самокат.
+       Локаторы FAQ привязаны к тексту вопроса, а не к хрупким ID."""
 
-    ORDER_BUTTON_UP = (By.XPATH, "//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать']") # кнопка Заказать наверху страницы
-    ORDER_BUTTON_DOWN = (By.XPATH, "//button[@class='Button_Button__ra12g Button_Middle__1CSJM']") # кнопка Заказать внизу страницы
+    # Кнопки заказа
+    ORDER_BUTTON_UP = (By.XPATH, "//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать']")
+    ORDER_BUTTON_DOWN = (By.XPATH, "//button[@class='Button_Button__ra12g Button_Middle__1CSJM']")
 
-    LOGO_SCOOTER = (By.CLASS_NAME, "Header_LogoScooter__3lsAR") # кнопка-логотип Самокат
-    LOGO_YANDEX = (By.CLASS_NAME, "Header_LogoYandex__3TSOI") # кнопка-логотип Яндекс
+    # Логотипы
+    LOGO_SCOOTER = (By.CLASS_NAME, "Header_LogoScooter__3lsAR")
+    LOGO_YANDEX = (By.CLASS_NAME, "Header_LogoYandex__3TSOI")
 
-    COOKIE_BANNER_CLOSE_BUTTON = (By.ID, "rcc-confirm-button") # кнопка закрытия баннера куки
+    # Кнопка закрытия баннера куки
+    COOKIE_BANNER_CLOSE_BUTTON = (By.ID, "rcc-confirm-button")
+    # --- ЛОКАТОРЫ ДЛЯ FAQ "Вопросы о важном" (УСТОЙЧИВЫЕ) ---
+    # Локаторы вопросов (по тексту)
+    FAQ_QUESTION_COST = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Сколько это стоит?')]")
+    FAQ_QUESTION_MULTIPLE_SCOOTERS = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Хочу сразу несколько самокатов!')]")
+    FAQ_QUESTION_RENTAL_TIME = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Как рассчитывается время аренды?')]")
+    FAQ_QUESTION_TODAY_ORDER = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Можно ли заказать самокат на сегодня?')]")
+    FAQ_QUESTION_EXTEND_RENT = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Можно ли продлить заказ?')]")
+    FAQ_QUESTION_CHARGING = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Вы привозите зарядку вместе с самокатом?')]")
+    FAQ_QUESTION_CANCEL = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Можно ли отменить заказ?')]")
+    FAQ_QUESTION_FAR_ZONE = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Я живу за МКАДом, привезёте?')]")
 
-    # раздел FAQ "Вопросы о важном", ВОПРОСЫ
-    FAQ_QUESTIONS = {
-        1: (By.ID, "accordion__heading-0"),
-        2: (By.ID, "accordion__heading-1"),
-        3: (By.ID, "accordion__heading-2"),
-        4: (By.ID, "accordion__heading-3"),
-        5: (By.ID, "accordion__heading-4"),
-        6: (By.ID, "accordion__heading-5"),
-        7: (By.ID, "accordion__heading-6"),
-        8: (By.ID, "accordion__heading-7")
-    }
-
-    # раздел FAQ "Вопросы о важном", ОТВЕТЫ
-    FAQ_ANSWERS = {
-        1: (By.ID, "accordion__panel-0"),
-        2: (By.ID, "accordion__panel-1"),
-        3: (By.ID, "accordion__panel-2"),
-        4: (By.ID, "accordion__panel-3"),
-        5: (By.ID, "accordion__panel-4"),
-        6: (By.ID, "accordion__panel-5"),
-        7: (By.ID, "accordion__panel-6"),
-        8: (By.ID, "accordion__panel-7")
-    }
+    # Локаторы ответов (находятся рядом с соответствующим вопросом)
+    FAQ_ANSWER_COST = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Сколько это стоит?')]/following-sibling::div[contains(@class, 'accordion__panel')]")
+    FAQ_ANSWER_MULTIPLE_SCOOTERS = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Хочу сразу несколько самокатов!')]/following-sibling::div[contains(@class, 'accordion__panel')]")
+    FAQ_ANSWER_RENTAL_TIME = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Как рассчитывается время аренды?')]/following-sibling::div[contains(@class, 'accordion__panel')]")
+    FAQ_ANSWER_TODAY_ORDER = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Можно ли заказать самокат на сегодня?')]/following-sibling::div[contains(@class, 'accordion__panel')]")
+    FAQ_ANSWER_EXTEND_RENT = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Можно ли продлить заказ?')]/following-sibling::div[contains(@class, 'accordion__panel')]")
+    FAQ_ANSWER_CHARGING = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Вы привозите зарядку вместе с самокатом?')]/following-sibling::div[contains(@class, 'accordion__panel')]")
+    FAQ_ANSWER_CANCEL = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Можно ли отменить заказ?')]/following-sibling::div[contains(@class, 'accordion__panel')]")
+    FAQ_ANSWER_FAR_ZONE = (By.XPATH, ".//div[contains(@class, 'accordion__heading') and contains(., 'Я живу за МКАДом, привезёте?')]/following-sibling::div[contains(@class, 'accordion__panel')]")
